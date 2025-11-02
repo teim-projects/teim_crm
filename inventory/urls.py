826f04bff8261da3e4b7054a70330a67d268ba3e
tanmay1413 <tanmaykulkarni1413@gmail.com>
2025-11-02 11:00:03 +0530

    add vendor.html page

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
-
+from .views import *
 urlpatterns = [
-    
+    path('add-vendor/', addVendor, name='add_vendor'),
+    path('vendor-list/', vendorList, name='vendor_list'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
 # Create your views here.
+
+def addVendor(request):
+  return render(request, 'inventory/add_vendor.html')
+
+
+def vendorList(request):
+  return render(request, 'inventory/vendor_list.html')
